b71f29f54fd46474c7dad75de8701f59f1350292
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-02-04 10:50:14 +0100

    test on room class: if room is overlapping return false

--- src/test/java/com/example/RoomTest.java
+++ src/test/java/com/example/RoomTest.java
@@ -10,7 +10,7 @@ public class RoomTest {
 
     // test on room class.
 
-    //test is room avalible
+    //test: is room avalible, room without bookings
     @Test
      void returnTrue_ifRoomIsAvalible() {
         Room room = new Room("room-1", "Room-2");
@@ -22,5 +22,43 @@ public class RoomTest {
         assertThat(result).isTrue();
     }
 
+    // test: is availble without overlaps
+    @Test
+    void returnFalse_ifRoomHasOverlappingBooking() {
+        Room room = new Room("room-1", "Room-2");
+
+        room.addBooking(booking(
+                "booking_1",
+                LocalDateTime.of(2026, 1, 1, 0, 0),
+                LocalDateTime.of(2026, 1, 3, 0, 0)
+        ));
+
+        boolean available = room.isAvailable(
+                LocalDateTime.of(2026, 1, 2, 0, 0),
+                LocalDateTime.of(2026, 1, 4, 0, 0)
+        );
+
+        assertThat(available).isFalse();
+    }
+
+    //test add booking and has booking
+
+
+
+    //test remove booking
+
+
+    // test get booking
+
+
+    //helper for tests
+    private Booking booking(String id, LocalDateTime start, LocalDateTime end) {
+        return new Booking(
+                id,
+                "room-1",
+                start,
+                end
+        );
+    }
 
 }
