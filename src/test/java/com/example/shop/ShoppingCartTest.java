68ee6462fe16a6b7a63f715a5259b4a671a92e5f
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-02-05 11:59:13 +0100

    test: cart increases when adding item to cart.

--- src/test/java/com/example/shop/ShoppingCartTest.java
+++ src/test/java/com/example/shop/ShoppingCartTest.java
@@ -2,14 +2,26 @@ package com.example.shop;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ShoppingCartTest {
 
+    // Test: total is zero when cart is still empty
     @Test
     void totalIsZero_whenCartIsEmpty() {
         ShoppingCart cart = new ShoppingCart();
 
         assertThat(cart.getTotal()).isEqualByComparingTo("0");
+    }
+
+    //test: cart increases when adding item to cart.
+    @Test
+    void totalIncreases_whenAddingItem(){
+        ShoppingCart cart = new ShoppingCart();
+        cart.add("Lamp", new BigDecimal(199));
+
+        assertThat(cart.getTotal()).isEqualByComparingTo("199");
     }
 }
